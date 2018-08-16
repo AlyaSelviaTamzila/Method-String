ba4974e0393237ef444683db5e8b8f0278c5e085
ASUS-A442U <ASUS-A442U@LAPTOP-0B7VJDRL>
2018-08-16 01:28:31 -0700

    Coba method length & isEmpty.

--- src/cobaoperasistring/CobaOperasiString.java
+++ src/cobaoperasistring/CobaOperasiString.java
@@ -5,5 +5,17 @@ public class CobaOperasiString {
     public static void main(String[] args) {
         String identitas = "ALYA SELVIA TAMZILA / X RPL 2 / 03";
         System.out.println("Identitas : "+ identitas);
+        
+        
+        String x ="operasi";
+        System.out.println("isi variable x : "+ x);
+        System.out.println("\""+ x + "\" panjang karakter = "+ x.length());
+        
+        System.out.println("x adalah kosong : "+ x.isEmpty());
+        
+        String y = "";
+        System.out.println("isi variable y : "+ y);
+        System.out.println("y adalah kosong : "+ y.isEmpty());
+        
     }
 }
